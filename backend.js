50b38791e110bb50df38c12f8d81d6fe3d0c6076
Spencer Cheney <spencer.cheney@ci.rexburg.id.us>
2019-12-05 17:42:50 -0700

    Added the lines into the placeholders. as well as toggle for the bubbles

--- backend.js
+++ backend.js
@@ -26,6 +26,12 @@ window.addEventListener('load', () => {
     ]
     createPlaceHolders();
     const pHolders = document.querySelectorAll(".placeHolders div");
+    //this array will keep track of where the bubble of each placeholder is in the sBubble array
+    var phFilled = [];
+    //initalize the phFilled array
+    for (let index = 0; index < 20; index++) {
+        phFilled.push(0);
+    }
 
     var bpm = 120;          //how fast the lines change in beats per minute
     var lineBeat = 1;       //line metronome starts here
@@ -116,42 +122,96 @@ window.addEventListener('load', () => {
                 offset = offset + 1 % 2;
             }
 
-            if((i - offset) % 2) {
-                ph.style.backgroundColor = phColors[0];
-            }
-            else {
-                ph.style.backgroundColor = phColors[1];
-            }
+            // if((i - offset) % 2) {
+            //     ph.style.backgroundColor = phColors[0];
+            // }
+            // else {
+            //     ph.style.backgroundColor = phColors[1];
+            // }
 
-            ph = null;
+            // ph.appendChild(createLine1());
+            // ph.appendChild(createLine2());
+            // ph.appendChild(createLine1());
+            // createLine1(ph);
+            // createLine2(ph);
+            // createLine1(ph);
+            const line = document.createElement("div");
+            ph.appendChild(line);
+            line.style.position = "relative";
+            line.style.left = "45%";
+            line.style.width = "10%";
+            line.style.height = "100%";
+            line.style.backgroundColor = "#000000";
+
+            // ph = null;
         }
     }
 
     //places bubble on a metronome line
     pHolders.forEach((ph, index) => {
         ph.addEventListener('click', function(){
+            var i = index;
+            // if(index != 0) {
+            //     i = index - 1;
+            // }
+            // else {
+            //     i = index;
+            // }
+            console.log(i);
             //if the placeholder doesn't have a bubble add one
-            if (sBubbles.children.length == 0) {
+            if (phFilled[i] == 0) {
                 const marked = document.createElement("div");
                 sBubbles.appendChild(marked);
                 //determines which pad the placeholder corresponds to
-                var padI = (index - (index % 4)) / 4;
+                var padI = (i - (i % 4)) / 4;
                 //sets the color to the color of the corresponding pad
                 marked.style.backgroundColor = padColors[padI];
                 //calculates the top location of the bubble and turns it into a string with a % at the end
                 var topLoc = 7.5 + (padI * 20);
                 var topLocS = topLoc.toString().concat("%");
                 //same thing but with the left location
-                var leftLoc = 10 + ((index % 4) * 25);
+                var leftLoc = 10 + ((i % 4) * 25);
                 var leftLocS = leftLoc.toString().concat("%");
                 marked.style.top = topLocS;
                 marked.style.left = leftLocS;
+                phFilled[i] = sBubbles.childElementCount ;
+                // console.log(sBubbles.childElementCount);
             }
 
             //if it does have a bubble remove it
             else {
-                sBubbles.removeChild(sBubbles.childNodes[0]);
+                sBubbles.removeChild(sBubbles.childNodes[phFilled[i] - 1]);
+                // console.log(phFilled[index]);
+                adjustFilledArray(phFilled[i]);
+                phFilled[i] = 0;
             }
         })
     })
+
+    //When a child of sBubbles is deleted the index of all of the children after that will decrease by one, this adjust phFilled accordingly
+    function adjustFilledArray(start) {
+        for (let index = 0; index < phFilled.length; index++) {
+            if (phFilled[index] >= start) {
+                phFilled[index] = phFilled[index] - 1;                
+            }
+        }        
+    }
+
+    function createLine1(ph) {
+        const line = document.createElement("div");
+        ph.appendChild(line);
+        line.style.width = "90%";
+        line.style.height = "100%";
+        line.style.backgroundColor = "#ffffff";
+        // return line;
+    }
+
+    function createLine2(ph) {
+        const line = document.createElement("div");
+        ph.appendChild(line);
+        line.style.width = "10%";
+        line.style.height = "100%";
+        line.style.backgroundColor = "#000000";
+        // return line;
+    }
 })
